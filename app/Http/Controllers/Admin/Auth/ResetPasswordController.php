27df47ce85314d265acdaf427e22e205502ee6bc
hallstar <hallstar01@gmail.com>
2020-12-30 18:42:46 -0500

    Implement password reset functionality

--- app/Http/Controllers/Admin/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Admin/Auth/ResetPasswordController.php
@@ -4,6 +4,13 @@ namespace App\Http\Controllers\Admin\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\ResetsPasswords;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Http\Request;
+use App\Constants\PasswordResetType;
+use App\Models\PasswordReset;
+use App\Models\AdminUser;
+use Inertia\Inertia;
+use Validator;
 
 class ResetPasswordController extends Controller
 {
@@ -35,5 +42,59 @@ class ResetPasswordController extends Controller
     public function __construct()
     {
         $this->middleware('guest');
+    }
+
+    public function showForm($token)
+    {
+
+        $password_reset = PasswordReset::where('token', $token)->where('type', PasswordResetType::ADMIN)->first();
+
+        if(empty($password_reset))
+        {
+            return redirect()->route('admin.password.request')->with('error', 'Invalid request token, please try again.');
+        }
+
+        return Inertia::render('Admin/Auth/ResetPassword', [
+            'account' => [
+                'email' => $password_reset->email,
+                'token' => $password_reset->token,
+            ]
+        ]);
+    }
+
+    public function reset(Request $request)
+    {
+
+        $validator = Validator::make($request->all(), [
+            'token' => 'required',
+            'email' => 'required|email',
+            'password' => 'required|min:8|confirmed',
+        ]);
+
+        if($validator->fails())
+        {
+            return redirect()->route('admin.password.reset', $request->token)->withInput()->withErrors($validator);
+        }
+
+        $password_reset = PasswordReset::where('token', $request->token)->where('type', PasswordResetType::ADMIN)->first();
+
+        if(empty($password_reset))
+        {
+            return redirect()->route('admin.password.reset', $request->token)->with('error', 'Invalid token.');
+        }
+
+        $account = AdminUser::where('email', $request->email)->first();
+
+        if(empty($account))
+        {
+            return redirect()->route('admin.password.reset', $request->token)->with('error', 'Invalid account');
+        }
+
+        $account->password = Hash::make($request->input('password'));
+        $account->save();
+
+        $password_reset->delete();
+
+        return redirect()->route('admin.login')->with('success', 'Password updated successfully.');
     }
 }
