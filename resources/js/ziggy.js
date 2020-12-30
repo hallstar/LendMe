35246e988d15e851c90d1487cd2028755c984489
hallstar <hallstar01@gmail.com>
2020-12-30 18:50:04 -0500

    Update ziggy routing for client app framework

--- resources/js/ziggy.js
+++ resources/js/ziggy.js
@@ -1,4 +1,4 @@
-const Ziggy = {"url":"http:\/\/localhost","port":null,"defaults":{},"routes":{"home":{"uri":"home","methods":["GET","HEAD"]},"comp":{"uri":"company","methods":["GET","HEAD"]},"admin.login":{"uri":"login","methods":["GET","HEAD"],"domain":"admin.lendme.test"},"admin.login.attempt":{"uri":"login","methods":["POST"],"domain":"admin.lendme.test"},"admin.logout":{"uri":"logout","methods":["POST"],"domain":"admin.lendme.test"},"admin.home":{"uri":"home","methods":["GET","HEAD"],"domain":"admin.lendme.test"}}};
+const Ziggy = {"url":"http:\/\/localhost","port":null,"defaults":{},"routes":{"admin.login":{"uri":"login","methods":["GET","HEAD"],"domain":"admin.lendme.test"},"admin.login.attempt":{"uri":"login","methods":["POST"],"domain":"admin.lendme.test"},"admin.logout":{"uri":"logout","methods":["POST"],"domain":"admin.lendme.test"},"admin.password.request":{"uri":"forgot-password","methods":["GET","HEAD"],"domain":"admin.lendme.test"},"admin.home":{"uri":"home","methods":["GET","HEAD"],"domain":"admin.lendme.test"}}};
 
 if (typeof window !== 'undefined' && typeof window.Ziggy !== 'undefined') {
     for (let name in window.Ziggy.routes) {
